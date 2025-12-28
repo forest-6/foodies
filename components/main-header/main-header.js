44a8d6114eae1f843b142ac2d39821111b628ac5
임승민 <seungmin3238@gmail.com>
2025-12-28 14:15:58 +0900

    메인 헤더 배경 컴포넌트로 분리

---
+++ components/main-header/main-header.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="NextLevel Food Logo" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
