f37c4fa758d3318f8e0b3ca98f7bf27dba25d837
임승민 <seungmin3238@gmail.com>
2026-01-03 16:48:36 +0900

    식사 공유 페이지 추가

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,25 @@
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicius Meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite meals from our collection of delicious recipes!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Recipes</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
