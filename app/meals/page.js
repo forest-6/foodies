2acb1d07204e22a4b6469cd1d6b84e31d9c79b62
임승민 <seungmin3238@gmail.com>
2026-01-03 17:48:09 +0900

    Suspense & Streamed Response를 이용한 세분화 로딩 상태 관리

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,14 @@ import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import { getMeals } from "@/libs/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default async function MealsPage() {
-  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +25,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Loading meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
