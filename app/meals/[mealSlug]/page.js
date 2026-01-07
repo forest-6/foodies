35848ad7e5cb4ef4443da97a37f37d854691b32c
임승민 <seungmin3238@gmail.com>
2026-01-07 21:31:21 +0900

    동적 메타데이터 추가

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -4,6 +4,17 @@ import { notFound } from "next/navigation";
 import classes from "./page.module.css";
 import { getMeal } from "@/libs/meals";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) notFound();
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
